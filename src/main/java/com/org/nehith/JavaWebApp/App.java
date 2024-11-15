d5c16dc56beac775405fdaf77793530886e8181c
Nehith <nehith456@gmail.com>
2024-11-15 11:14:28 +0530

    commited

---
+++ src/main/java/com/org/nehith/JavaWebApp/App.java
@@ -0,0 +1,10 @@
+package com.org.nehith.JavaWebApp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
